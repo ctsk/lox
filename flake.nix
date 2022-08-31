69a872fe2d00a7ec67323ba373b8ae2565db4116
ctsk <9384305+ctsk@users.noreply.github.com>
2022-08-31 21:21:03 +0200

    Add Nix env for Java and C

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Crafting Interpreters";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system: 
+
+      let 
+        pkgs = import nixpkgs { inherit system; };
+
+        gcc = pkgs.gcc;
+        openjdk = pkgs.openjdk;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            gcc
+            openjdk
+          ];
+        };
+      }
+    );
+}
